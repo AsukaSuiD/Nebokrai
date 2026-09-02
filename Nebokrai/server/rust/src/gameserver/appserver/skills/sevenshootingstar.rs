0892145a5d68e9174c0d17b8c2bd7020d086e10d
AsukaSuiD <AsukaSuiD@gmail.com>
2026-09-02 07:10:58 +0300

    Восстановить x87-критику семи падающих звёзд

--- Nebokrai/server/rust/src/gameserver/appserver/skills/sevenshootingstar.rs
+++ Nebokrai/server/rust/src/gameserver/appserver/skills/sevenshootingstar.rs
@@ -233,7 +233,9 @@ fn calculate_attack(
         attack.critical = true;
         let critical_rate = game.globe_setup().critical_rate();
         for power in &mut attack.damages {
-            power.hp_damage = (power.hp_damage as f32 * critical_rate) as i32;
+            power.hp_damage = truncate_original(
+                f64::from(power.hp_damage) * f64::from(critical_rate),
+            );
         }
     }
     Some((master, attack))
